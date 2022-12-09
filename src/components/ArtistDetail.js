ca4decdf2bfaa53777f0415b6a4f6c4f9dde1ab2
Banu Seyhan <banuaskahraman@gmail.com>
2022-12-09 22:47:20 +0300

    ok

--- src/components/ArtistDetail.js
+++ src/components/ArtistDetail.js
@@ -6,7 +6,7 @@ import Track from "./Track";
 function ArtistDetail() {
   const location = useLocation();
   return (
-
+    <div>
         
     <div className="container h-100 py-5">
       <div className="row border rounded">
@@ -15,7 +15,9 @@ function ArtistDetail() {
             <div className="card-body p-4" >
               <div className="row d-flex justify-content-between align-items-center">
                 <div className="col-md-2 col-lg-2 col-xl-2">
-                 
+                  <img
+                    className="img-fluid rounded-3" src={location.state.url}/>
+                </div>
                 <div className="col-md-10 col-lg-10 col-xl-10 text-start">
                   <p className="text-center text-sm-center text-lg-start">
                     <span><b>{location.state.name}</b></span>
